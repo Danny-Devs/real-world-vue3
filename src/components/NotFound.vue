4db8ce059ef107b1ce15986ac391d64ea745253d
Daniel Ahn <et3rnal.d@gmail.com>
2022-05-03 21:58:57 -0400

    add catch all path and 404 component

---
+++ src/components/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Error 404. Page not found.</h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import EventDetailView from '@/views/EventDetailView.vue'
+import NotFound from '@/components/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,8 @@ const router = createRouter({
     },
     // dynamic segments start with a colon
     { path: '/event/:id', name: 'EventDetailView', props: true, component: EventDetailView },
+    { path: '*',
+    component: NotFound}
   ],
 })
 
